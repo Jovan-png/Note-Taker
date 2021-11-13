043cae8b169b14e3f69026c6548c83c3aebd0bd4
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-11-13 11:35:42 -0600

    added ability to post new notes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const {notes} = require('./db/db.json')
 const path = require('path')
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json())
+
 
 app.get('/api/notes', (req, res)=>{
     res.json(notes)
@@ -13,6 +15,17 @@ app.get('/notes', (req, res)=>{
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+app.post('/api/notes', (req,res)=>{
+     const newNote = {
+         title: req.body.title,
+         text: req.body.text
+     }
+     if(!newNote.text || !newNote.text){
+         res.status(400).json({msg: ERROR})
+     }
+     notes.push(newNote)
+     res.json(newNote)
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('*', (req,res)=>{
