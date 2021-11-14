49c22c5820acba08e860a7aa293769261c7559fa
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-11-13 18:17:36 -0600

    finished implementing put

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const {notes} = require('./db/db.json')
 const path = require('path')
+const fs = require('fs')
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
@@ -21,12 +22,25 @@ app.get('/api/notes/:id', (req, res)=>{
 })
 
 app.delete('/api/notes/:id', (req, res)=>{
-
+const delNote = notes.some(notes => notes.id === parseInt(req.params.id))
+if(delNote){
     res.json(notes.filter(notes => notes.id !== parseInt(req.params.id)))
-
-
-    
+    }
 })
+
+app.put('/api/notes/:id', (req, res)=>{
+
+        notes.forEach(notes =>{
+            if(notes.id === req.params.id){
+                notes.title = req.body.title;
+                notes.text = req.body.text;
+
+                res.json('Hello')
+            }
+        })
+})
+
+// Creates new Note with unique ID.
 app.post('/api/notes', (req,res)=>{
      const newNote = {
          id: req.body.id = notes.length.toString(),
@@ -41,6 +55,8 @@ app.post('/api/notes', (req,res)=>{
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Makes the default page "index.html"
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
